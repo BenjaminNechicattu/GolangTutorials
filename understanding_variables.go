0d1ff0f61937180c356bf37923536013b1f68118
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2020-10-30 00:44:57 +0530

    Update understanding_variables.go

--- understanding_variables.go
+++ understanding_variables.go
@@ -3,7 +3,7 @@ import "fmt"
 
 func main() {
 	var varByte byte = 'a'
-	var varRune rune = '🎞'
-
-	fmt.Printf("%c = %d and %c = %U\n", varByte, varByte, varRune, varRune)
+	var varRune rune = '♥'
+	var  a = 100
+	fmt.Printf("%c = %d and %c = %U\n also a = %d", varByte, varByte, varRune, varRune, a)
 }
