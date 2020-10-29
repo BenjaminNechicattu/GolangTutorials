cf214a3df948fa241b3c43a8d185b76930460440
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2020-10-30 01:08:24 +0530

    Create operators.go

---
+++ operators.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+    var a int=20  //declariing two integers
+    var b int=10
+    
+    fmt.Println("Go Arithmetic Operators")
+    
+    
+    fmt.Println(a+b) // addition
+    fmt.Println(a-b) // subtraction
+    fmt.Println(a*b) // multiplication
+    fmt.Println(a/b) // division
+    fmt.Println(a%b) //modulo
+   
+}
