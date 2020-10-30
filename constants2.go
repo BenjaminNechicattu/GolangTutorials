15c11178c589330830abfe5c793effbf1131144c
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2020-10-30 13:09:02 +0530

    Create constants2.go

---
+++ constants2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  // constants is simly imutable values. created like a variable but const instead of var keyword
+  // syntax : const <constant_name> = value
+
+  const x string = "Hello World"
+  fmt.Println(x)
+}
