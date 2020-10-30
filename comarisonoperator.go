e19d397aa7a9d64969d3f2d44a362252271aa1c8
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2020-10-30 12:33:57 +0530

    Create comarisonoperator.go

---
+++ comarisonoperator.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+    var a int =20
+    var b int =10
+
+    if (a > b) {
+        fmt.Println("a is greater than b.")
+    } else{
+        fmt.Println("b is greater than a.")
+    }
+}
