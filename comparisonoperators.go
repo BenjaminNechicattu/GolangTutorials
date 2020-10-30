f34b61fbadce17c702e33915545847183d093f9c
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2020-10-30 12:36:35 +0530

    Update and rename comarisonoperator.go to comparisonoperators.go

--- comparisonoperators.go
+++ comparisonoperators.go
@@ -3,7 +3,9 @@ import "fmt"
 func main() {
     var a int =20
     var b int =10
-
+    // all comparison operators <, >, >=,<=, ==, !=
+    //comsider an example
+    
     if (a > b) {
         fmt.Println("a is greater than b.")
     } else{
