ec88ffdddb574a233ce9c7da58a6054ced70eeb9
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2020-10-31 14:06:16 +0530

    Update and rename forloop.go to ifcondition.go

--- ifcondition.go
+++ ifcondition.go
@@ -4,13 +4,12 @@ func main() {
     var x = 25
     
     /*
-    
     if(condation){  
     statement(s) to be executed when condition is true
     }
     */
     
-    fmt.Println("W3Adda - Go If Statement")
+    fmt.Println("Go If Statement")
     if(x > 10) {
         fmt.Printf("%d is greater than 10\n", x)
     }
