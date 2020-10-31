f201b7f2a6c32e190282cd0916646a15075b8303
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2020-10-31 14:05:01 +0530

    Create forloop.go

---
+++ forloop.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+func main() {
+    var x = 25
+    
+    /*
+    
+    if(condation){  
+    statement(s) to be executed when condition is true
+    }
+    */
+    
+    fmt.Println("W3Adda - Go If Statement")
+    if(x > 10) {
+        fmt.Printf("%d is greater than 10\n", x)
+    }
+}
