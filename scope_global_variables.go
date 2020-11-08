23ef6aa1c6168df24759e706d7eec6e7ab983d79
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2020-11-08 23:11:43 +0530

    Create scope_global_variables.go

---
+++ scope_global_variables.go
@@ -0,0 +1,19 @@
+Live Demo
+package main
+
+import "fmt"
+ 
+/* global variable declaration */
+var g int
+ 
+func main() {
+   /* local variable declaration */
+   var a, b int
+
+   /* actual initialization */
+   a = 10
+   b = 20
+   g = a + b
+
+   fmt.Printf("value of a = %d, b = %d and g = %d\n", a, b, g)
+}
