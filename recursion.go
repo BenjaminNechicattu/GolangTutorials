3773d9e66864f2ab2d1e3d89f2c28aceb1e146cb
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2020-11-08 23:25:16 +0530

    Create recursion.go

---
+++ recursion.go
@@ -0,0 +1,25 @@
+/* syntax : 
+      func recursion() {
+         recursion() /* function calls itself */
+      }
+      func main() {
+         recursion()
+}
+*/
+
+// factorial
+
+package main
+
+import "fmt"
+
+func factorial(i int)int {
+   if(i <= 1) {
+      return 1
+   }
+   return i * factorial(i - 1)
+}
+func main() { 
+   var i int = 15
+   fmt.Printf("Factorial of %d is %d", i, factorial(i))
+}
